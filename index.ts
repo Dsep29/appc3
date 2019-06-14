84b3a8d0259525aa5fbd5feb7531a7d34514f4b8
Diego <diego.sepulveda.j@gmail.com>
2019-06-14 09:41:46 -0400

    prueba ts proc and pack

--- index.ts
+++ index.ts
@@ -45,8 +45,8 @@ server.app.use('/material', materialRoutes );
 
 
 // Conectar DB mongodb+srv://Diego:Avondale15@fepasa-j0qfp.mongodb.net/test?retryWrites=true&w=majority
-//  
-mongoose.connect('mongodb://localhost:27017/fepasa', 
+//  mongodb://localhost:27017/fepasa
+mongoose.connect('mongodb+srv://Diego:Avondale15@fepasa-j0qfp.mongodb.net/test?retryWrites=true&w=majority', 
                 { useNewUrlParser: true, useCreateIndex: true }, ( err ) => {
 
    if ( err ) throw  console.log(err);
