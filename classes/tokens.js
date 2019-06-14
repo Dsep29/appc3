befce80c235c3681dad185dcfad09acdb24442a6
Diego <diego.sepulveda.j@gmail.com>
2019-06-14 13:43:54 -0400

    tokens

--- classes/tokens.js
+++ classes/tokens.js
@@ -4,8 +4,8 @@ const jwt = require('jsonwebtoken');
 // export default class Token {
     module.exports =  function(Token) {
 
-    private seed = 'este-es-el-seed-de-mi-app-secreto';
-    private caducidad = '30d';
+    private const seed = 'este-es-el-seed-de-mi-app-secreto';
+    private const caducidad = '30d';
 
     getJwtToken( payload: any ): string {
         return jwt.sign({
