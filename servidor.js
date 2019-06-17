ba616f63efc45cab90cd3b8af4b1a6d45f76b854
Diego <diego.sepulveda.j@gmail.com>
2019-06-17 11:50:10 -0400

    cambios servidorjs and pack

--- servidor.js
+++ servidor.js
@@ -1,10 +1,11 @@
-const express = require('index');
-const http = require('http');
-const mongoose = require('mongoose');
-const path = require('path');
+require('index');
+// const express = require('index');
+// const http = require('http');
+// const mongoose = require('mongoose');
+// const path = require('path');
 
-const app = express();
-const server = http.createServer(app);
+// const app = express();
+// const server = http.createServer(app);
 
 // rutas
  // const userRoutes = require('./routes/usuarios');
@@ -20,25 +21,25 @@ const server = http.createServer(app);
 //  server.app.use('/orden', ordenRoutes );
 //  server.app.use('/material', materialRoutes ); mongodb://localhost:27017/fepasa
  //  mongodb+srv://Diego:Avondale15@fepasa-j0qfp.mongodb.net/test?retryWrites=true&w=majority
-mongoose.connect('mongodb+srv://Diego:Avondale15@fepasa-j0qfp.mongodb.net/test?retryWrites=true&w=majority',
-                  { useNewUrlParser: true, useCreateIndex: true })
-                .then( db => console.log( 'base de datos conectada', db))
-                .catch( err => console.log(err));
+// mongoose.connect('mongodb+srv://Diego:Avondale15@fepasa-j0qfp.mongodb.net/test?retryWrites=true&w=majority',
+//                   { useNewUrlParser: true, useCreateIndex: true })
+//                 .then( db => console.log( 'base de datos conectada', db))
+//                 .catch( err => console.log(err));
 
-app.set('port', process.env.PORT || 3000);
+// app.set('port', process.env.PORT || 3000);
 
-app.use(express.static(path.join(__dirname, 'index')));
+// app.use(express.static(path.join(__dirname, 'index')));
 
-server.listen(app.get('port'), ()=>{
-    console.log(`server port: ${app.get('port')}` );    
-});
+// server.listen(app.get('port'), ()=>{
+//     console.log(`server port: ${app.get('port')}` );    
+// });
 
 
-// inicio
-app.get("/", function(req, res) {
-    //when we get an http get request to the root/homepage
-    res.send("Hello World");
-  });
+// // inicio
+// app.get("/", function(req, res) {
+//     //when we get an http get request to the root/homepage
+//     res.send("Hello World");
+//   });
 
   
 
